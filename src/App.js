89d51db74963cfcdf382bb53eea63ce9b5614874
andreiursan23 <ursanandrei@ymail.com>
2021-11-08 21:46:39 +0200

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.scss";
 
 function App() {
+  // States for calculator operations
   const [number, setNumber] = useState(0);
-  const [memory, setMemory] = useState(0);
   const [result, setResult] = useState(0);
   const [operation, setOperation] = useState(null);
-  const [previousOperation, setPreviousOperation] = useState("");
 
+  // States for color themes
+  const [selected, setSelected] = useState(1);
+  const [theme, setTheme] = useState("theme1");
+
+  useEffect(() => {
+    if (selected === 1) {
+      setTheme("theme1");
+    } else if (selected === 2) {
+      setTheme("theme2");
+    } else if (selected === 3) {
+      setTheme("theme3");
+    }
+  }, [selected]);
+
+  // Helper functions for calculator operations
   const getNumber = (inputNumber) => {
     if (!number) {
       setNumber(inputNumber);
@@ -16,133 +30,68 @@ function App() {
     }
   };
 
-  const add = () => {
-    if (operation !== null) {
+  const calculations = () => {
+    if (operation !== null && result !== 0) {
       if (operation === "+") {
         setResult(result + number);
       } else if (operation === "-") {
-        setResult(result + number);
+        setResult(result - number);
       } else if (operation === "*") {
-        setResult(result + number);
+        setResult(result * number);
       } else if (operation === "/") {
-        setResult(result + number);
+        if (number !== 0) {
+          setResult(result / number);
+        } else {
+          setResult("Error");
+        }
       }
     } else {
       setResult(number);
     }
     setNumber(0);
-    setOperation("+");
-  };
-
-  const substract = () => {
-    if (operation !== null) {
-      if (operation === "+") {
-        setResult(result + number);
-      } else if (operation === "-") {
-        setResult(result + number);
-      } else if (operation === "*") {
-        setResult(result + number);
-      } else if (operation === "/") {
-        setResult(result + number);
-      }
-    } else {
-      setResult(number);
-    }
-    setNumber(0);
-    setOperation("-");
-  };
-
-  const multiply = () => {
-    if (operation !== null) {
-      if (operation === "+") {
-        setResult(result + number);
-      } else if (operation === "-") {
-        setResult(result + number);
-      } else if (operation === "*") {
-        setResult(result + number);
-      } else if (operation === "/") {
-        setResult(result + number);
-      }
-    } else {
-      setResult(number);
-    }
-    setNumber(0);
-    setOperation("*");
-  };
-
-  const divide = () => {
-    if (operation !== null) {
-      if (operation === "+") {
-        setResult(result + number);
-      } else if (operation === "-") {
-        setResult(result + number);
-      } else if (operation === "*") {
-        setResult(result + number);
-      } else if (operation === "/") {
-        setResult(result + number);
-      }
-    } else {
-      setResult(number);
-    }
-    setNumber(0);
-    setOperation("/");
   };
 
   const handleOperation = (operationType) => {
-    if (operation !== operationType) {
-      switch (operationType) {
-        case "+":
-          add();
-          break;
-        case "-":
-          substract();
-          break;
-        case "/":
-          divide();
-          break;
-        case "*":
-          multiply();
-          break;
-        default:
-          return;
-      }
-    } else {
-      switch (operationType) {
-        case "+":
-          add();
-          break;
-        case "-":
-          substract();
-          break;
-        case "/":
-          divide();
-          break;
-        case "*":
-          multiply();
-          break;
-        default:
-          return;
-      }
+    switch (operationType) {
+      case "+":
+        calculations();
+        setOperation("+");
+        break;
+      case "-":
+        calculations();
+        setOperation("-");
+        break;
+      case "/":
+        calculations();
+        setOperation("/");
+
+        break;
+      case "*":
+        calculations();
+        setOperation("*");
+        break;
+      default:
+        return;
     }
   };
 
-  const arithmeticOperation = (type) => {
-    setOperation(type);
-  };
-
   const getResult = () => {
-    handleOperation(operation);
-  };
-
-  const reset = () => {
-    setResult(0);
-    setNumber(0);
-    setMemory(0);
+    calculations();
     setOperation("");
   };
 
+  const reset = () => {
+    setOperation("");
+    setResult(0);
+    setNumber(0);
+  };
+
   const deleteNumber = () => {
-    setNumber(number.toString().slice(0, -1));
+    if (number === 0 || number === "") {
+      setNumber(0);
+    } else {
+      setNumber(number.toString().slice(0, -1));
+    }
   };
 
   const setDecimal = () => {
@@ -157,41 +106,130 @@ function App() {
     return num_parts.join(".");
   };
 
+  // Color themes helpers
+  const toggleValues = [1, 2, 3];
+
+  const handleChange = (val) => {
+    setSelected(val);
+  };
+
   return (
     <>
-      <div className="container">
-        <span>
-          Result: {result} Number: {number} Memory: {memory} Operation:
-          {operation}
-        </span>
-        <br />
-        <br />
-        <span>
-          {number ? thousandsSeparators(number) : thousandsSeparators(result)}
-        </span>
-        <div className="inputs-container">
-          <button onClick={() => getNumber(7)}>7</button>
-          <button onClick={() => getNumber(8)}>8</button>
-          <button onClick={() => getNumber(9)}>9</button>
-          <button onClick={deleteNumber}>DEL</button>
-          <button onClick={() => getNumber(4)}>4</button>
-          <button onClick={() => getNumber(5)}>5</button>
-          <button onClick={() => getNumber(6)}>6</button>
-          <button onClick={() => handleOperation("+")}>+</button>
-          <button onClick={() => getNumber(1)}>1</button>
-          <button onClick={() => getNumber(2)}>2</button>
-          <button onClick={() => getNumber(3)}>3</button>
-          <button onClick={() => handleOperation("-")}>-</button>
-          <button onClick={setDecimal}>.</button>
-          <button onClick={() => getNumber(0)}>0</button>
-          <button onClick={() => handleOperation("/")}>/</button>
-          <button onClick={() => handleOperation("*")}>*</button>
-          <button className="grown-inputs" onClick={reset}>
-            RESET
-          </button>
-          <button className="grown-inputs" onClick={getResult}>
-            =
-          </button>
+      <div className={`container ${theme}`}>
+        <div className="calculator-container">
+          <div className="theme-switch ">
+            <h1>calc</h1>
+            <div className="theme">
+              <h2>THEME</h2>
+              <div className={`switch-container ${theme}`}>
+                {toggleValues.map((val, index) => (
+                  <span key={index}>
+                    <input
+                      selected={selected}
+                      type="radio"
+                      name="switch"
+                      checked={selected === val}
+                      className={`switch-radio ${theme}`}
+                    />
+                    <label
+                      title={val}
+                      onClick={() => handleChange(val)}
+                      className={`switch-label ${theme}`}
+                    >
+                      {val}
+                    </label>
+                  </span>
+                ))}
+                <span
+                  style={{
+                    left: `${(toggleValues.indexOf(selected) / 3) * 100}%`,
+                  }}
+                  className={`switch-selection ${theme}`}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={`display ${theme}`}>
+            <p>
+              {number
+                ? thousandsSeparators(number)
+                : thousandsSeparators(result)}
+            </p>
+          </div>
+          <div className={`inputs-container ${theme}`}>
+            <button onClick={() => getNumber(7)} className={`${theme}`}>
+              7
+            </button>
+            <button onClick={() => getNumber(8)} className={`${theme}`}>
+              8
+            </button>
+            <button onClick={() => getNumber(9)} className={`${theme}`}>
+              9
+            </button>
+            <button onClick={deleteNumber} className={`blue-inputs ${theme}`}>
+              DEL
+            </button>
+            <button onClick={() => getNumber(4)} className={`${theme}`}>
+              4
+            </button>
+            <button onClick={() => getNumber(5)} className={`${theme}`}>
+              5
+            </button>
+            <button onClick={() => getNumber(6)} className={`${theme}`}>
+              6
+            </button>
+            <button
+              onClick={() => handleOperation("+")}
+              className={`operations ${theme}`}
+            >
+              +
+            </button>
+            <button onClick={() => getNumber(1)} className={`${theme}`}>
+              1
+            </button>
+            <button onClick={() => getNumber(2)} className={`${theme}`}>
+              2
+            </button>
+            <button onClick={() => getNumber(3)} className={`${theme}`}>
+              3
+            </button>
+            <button
+              onClick={() => handleOperation("-")}
+              className={`operations ${theme}`}
+            >
+              -
+            </button>
+            <button onClick={setDecimal} className={`operations ${theme}`}>
+              .
+            </button>
+            <button onClick={() => getNumber(0)} className={`${theme}`}>
+              0
+            </button>
+            <button
+              onClick={() => handleOperation("/")}
+              className={`operations ${theme}`}
+            >
+              ÷
+            </button>
+            <button
+              onClick={() => handleOperation("*")}
+              className={`operations ${theme}`}
+            >
+              ×
+            </button>
+            <button
+              className={`grown-inputs blue-inputs ${theme}`}
+              onClick={reset}
+            >
+              RESET
+            </button>
+            <button
+              className={`grown-inputs red-inputs ${theme}`}
+              onClick={getResult}
+            >
+              =
+            </button>
+          </div>
         </div>
       </div>
     </>
